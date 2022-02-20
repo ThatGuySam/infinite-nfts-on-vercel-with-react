a7c4266b7d57f6da4e02adae147b1e2e739c7cdf
Sam Carlton <sam@sam.lc>
2022-02-20 15:03:18 -0600

    Parse and shuffle image urls

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -2,9 +2,13 @@ import fs from 'fs-extra'
 import wordListPath from 'word-list'
 // https://github.com/webcaetano/shuffle-seed
 import shuffleSeed from 'shuffle-seed'
+import { XMLParser } from 'fast-xml-parser'
 
 import { getDeterministicItem } from '../src/helpers/deterministic.js'
+import { parseUnsplashImgixUrl } from '../src/helpers/urls.js'
 
+
+const xmlParser = new XMLParser()
 
 
 async function shuffleAndSaveToText ( options = {} ) {
@@ -27,6 +31,14 @@ async function shuffleAndSaveToText ( options = {} ) {
     return
 }
 
+const paths = {}
+
+paths.gitIgnoredPrefix = 'built'
+paths.assets = './src/assets'
+paths.wordsList = `${ paths.assets }/${ paths.gitIgnoredPrefix }-words`
+paths.imageUrlXml = `${ paths.assets }/image-urls.xml`
+paths.imageUrlList = `${ paths.assets }/${ paths.gitIgnoredPrefix }-image-urls`
+
 
 async function main() {
 
@@ -40,17 +52,50 @@ async function main() {
     await shuffleAndSaveToText({
         list: alphaWords,
         seedNumber: numberForShuffle,
-        savePath: './src/assets/built-words'
+        savePath: paths.wordsList
     })
 
 
     const sampleWord = await getDeterministicItem({
-        list: './src/assets/words.txt',
+        list: `${ paths.wordsList }.txt`, 
         listLength: alphaWords.length,
         seedNumber: numberForShuffle
     })
 
     console.log('Sample word:', sampleWord)
+
+
+    // Get our image xml
+    console.log('Getting image xml...')
+    const imageXML = await fs.readFile( paths.imageUrlXml, 'utf8')
+    const {
+        urlset
+    } = xmlParser.parse(imageXML)
+
+    // Map the image urls to an array
+    const imageURLs = urlset.url.map(url => {
+        const imgixUrl = url['image:image']['image:loc']
+        return parseUnsplashImgixUrl(imgixUrl)
+    })
+
+    console.log('Shuffling image urls...')
+    await shuffleAndSaveToText({
+        list: imageURLs,
+        seedNumber: numberForShuffle,
+        savePath: paths.imageUrlList
+    })
+
+    const sampleImageUrl = await getDeterministicItem({
+        list: `${ paths.imageUrlList }.txt`, 
+        listLength: imageURLs.length,
+        seedNumber: numberForShuffle
+    })
+
+    console.log('Sample image url:', sampleImageUrl)
+
+
+    // Save paths to meta data
+    await fs.writeJSON(`${ paths.assets }/${ paths.gitIgnoredPrefix }-path-references.json`, paths)
 
 
     console.log('Build completed')
@@ -59,4 +104,3 @@ async function main() {
 }
 
 main()
-    .then(() => process.exit(0))
