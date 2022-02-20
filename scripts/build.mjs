2e0174c58c547c2d886305cac34dc288958d2a57
Sam Carlton <sam@sam.lc>
2022-02-20 13:45:36 -0600

    Add prebuild script

---
+++ scripts/build.mjs
@@ -0,0 +1,62 @@
+import fs from 'fs-extra'
+import wordListPath from 'word-list'
+// https://github.com/webcaetano/shuffle-seed
+import shuffleSeed from 'shuffle-seed'
+
+import { getDeterministicItem } from '../src/helpers/deterministic.mjs'
+
+
+
+async function shuffleAndSaveToText ( options = {} ) {
+    const {
+        list, 
+        seedNumber,
+        savePath
+    } = options
+
+    const shuffledList = shuffleSeed.shuffle(list, seedNumber)
+
+    // Write the data to a file
+    await fs.writeFile(`${savePath}.txt` , shuffledList.join('\n'))
+
+    // Save the meta data as well
+    await fs.writeJSON(`${savePath}-meta.json`, {
+        total: shuffledList.length,
+    })
+
+    return
+}
+
+
+async function main() {
+
+    // console.log( 'wordListPath', wordListPath )
+    const alphaWords = (await fs.readFile(wordListPath, 'utf8')).split('\n')
+
+    // Seed number to set the default order of all words
+    const numberForShuffle = 4815162342
+
+    console.log('Shuffling words...')
+    await shuffleAndSaveToText({
+        list: alphaWords,
+        seedNumber: numberForShuffle,
+        savePath: './src/assets/words'
+    })
+
+
+    const sampleWord = await getDeterministicItem({
+        list: './src/assets/words.txt',
+        listLength: alphaWords.length,
+        seedNumber: numberForShuffle
+    })
+
+    console.log('Sample word:', sampleWord)
+
+
+    console.log('Build completed')
+
+    process.exit()
+}
+
+main()
+    .then(() => process.exit(0))
