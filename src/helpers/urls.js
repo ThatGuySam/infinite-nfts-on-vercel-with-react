72ca8bcbd4ffa5a3e74eb4ecb0e5721ead255d33
Sam Carlton <sam@sam.lc>
2022-02-20 14:09:11 -0600

    Add parseUnsplashImgixUrl helper

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -19,3 +19,19 @@ export function getImageUrl ( hexidecimalTokenId ) {
 
     return `nft-media/${ decimal }.svg`
 }
+
+export function parseUnsplashImgixUrl ( rawImgixUrl ) {
+    const url = new URL( rawImgixUrl )
+
+    // Unsplash uses Imgix so so update the Imgix params to want we want
+    // Example - https://images.unsplash.com/photo-1643544590582-33dc3477a3cd?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces,focalpoint&cs=tinysrgb&w=1080&h=1080&fit=crop
+
+    // https://docs.imgix.com/apis/rendering/size/crop
+    url.searchParams.set('crop', 'faces,focalpoint')
+    // https://docs.imgix.com/apis/rendering/size/fit
+    url.searchParams.set('fit', 'crop')
+    url.searchParams.set('w', 1080)
+    url.searchParams.set('h', 1080)
+
+    return url.toString()
+}
