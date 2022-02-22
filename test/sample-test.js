a995d3271e6442304b3247064ebfc45bdaf14fb1
Sam Carlton <sam@sam.lc>
2022-02-21 18:31:26 -0600

    Fix test not working with import

--- test/sample-test.js
+++ test/sample-test.js
@@ -6,19 +6,19 @@ const { ethers } = require("hardhat");
 
 describe("MyNFT", function () {
   it("Should mint and transfer an NFT to someone", async function () {
-    const { getMetaDataUrl } = await import('../src/helpers/urls.mjs')
+    // const { getMetaDataUrl } = await import('../src/helpers/urls.js')
 
     const FiredGuys = await ethers.getContractFactory("FiredGuys");
     const firedguys = await FiredGuys.deploy();
     await firedguys.deployed()
 
     const recipient = '0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199'
-    const metadataURI = getMetaDataUrl( recipient )
+    const metadataURI = `nft-media/${ recipient }.json`//getMetaDataUrl( recipient )
 
     let balance = await firedguys.balanceOf(recipient);
     expect(balance).to.equal(0);
 
-    const newlyMintedToken = await firedguys.payToMint(recipient, metadataURI, { value: ethers.utils.parseEther('0.05') });
+    const newlyMintedToken = await firedguys.payToMint(recipient, metadataURI, { value: ethers.utils.parseEther('0.005') });
 
     // wait until the transaction is mined
     await newlyMintedToken.wait();
@@ -27,6 +27,6 @@ describe("MyNFT", function () {
     expect(balance).to.equal(1);
 
     expect(await firedguys.isContentOwned(metadataURI)).to.equal(true);
-    const newlyMintedToken2 = await firedguys.payToMint(recipient, 'foo', { value: ethers.utils.parseEther('0.05') });
+    const newlyMintedToken2 = await firedguys.payToMint(recipient, 'foo', { value: ethers.utils.parseEther('0.005') });
   });
 });
